756ee877a803293439d5536e610640377a4a8881
williamsonpg <williamsonpg@state.gov>
2021-03-01 13:18:59 -0500

    implement testing

--- test/sampleAPI.test/UnitTest1.cs
+++ test/sampleAPI.test/UnitTest1.cs
@@ -5,6 +5,13 @@ using sampleAPI.Controllers;
 namespace sampleAPI.test
 {
 // 51bd7aa8f32afab78b971070f1c2f2e77680f0d0
+/*
+ * git status
+ * git add .
+ * git commit -m "nature of change"
+ * git push origin master  
+ * 
+ */
     public class UnitTest1
     {
         WeatherForecastController sapiCntrl = new WeatherForecastController(null);
